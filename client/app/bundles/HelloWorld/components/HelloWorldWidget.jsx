16962396c582f8a2514f66f2a27d578fffa3455a
Justin Gordon <justin.gordon@gmail.com>
2016-01-04 22:44:03 -0800

    Formatting changes and make dumb component dumber

--- client/app/bundles/HelloWorld/components/HelloWorldWidget.jsx
+++ client/app/bundles/HelloWorld/components/HelloWorldWidget.jsx
@@ -2,9 +2,8 @@
 // all your dump component names with Widget.
 
 import React, { PropTypes } from 'react';
-import Immutable from 'immutable';
 import _ from 'lodash';
-import { Input, Row, Col } from 'react-bootstrap';
+import { Input } from 'react-bootstrap';
 
 // Simple example of a React "dumb" component
 export default class HelloWorldWidget extends React.Component {
@@ -12,10 +11,8 @@ export default class HelloWorldWidget extends React.Component {
     // We prefix all property and variable names pointing to Immutable.js objects with '$$'.
     // This allows us to immediately know we don't call $$helloWorldStore['someProperty'], but
     // instead use the Immutable.js `get` API for Immutable.Map
-    actions: PropTypes.shape({
-      updateName: PropTypes.func.isRequired,
-    }).isRequired,
-    $$helloWorldStore: PropTypes.instanceOf(Immutable.Map).isRequired,
+    updateName: PropTypes.func.isRequired,
+    name: PropTypes.string.isRequired,
   };
 
   constructor(props, context) {
@@ -24,18 +21,17 @@ export default class HelloWorldWidget extends React.Component {
     // Uses lodash to bind all methods to the context of the object instance, otherwise
     // the methods defined here would not refer to the component's class, not the component
     // instance itself.
-    _.bindAll(this, '_handleChange');
+    _.bindAll(this, 'handleChange');
   }
 
   // React will automatically provide us with the event `e`
-  _handleChange(e) {
+  handleChange(e) {
     const name = e.target.value;
-    this.props.actions.updateName(name);
+    this.props.updateName(name);
   }
 
   render() {
-    const $$helloWorldStore = this.props.$$helloWorldStore;
-    const name = $$helloWorldStore.get('name');
+    const { name } = this.props;
     return (
       <div>
         <h3>
@@ -43,12 +39,14 @@ export default class HelloWorldWidget extends React.Component {
         </h3>
         <hr/>
         <form className="form-horizontal">
-            <Input type="text"
-                   labelClassName="col-sm-2"
-                   wrapperClassName="col-sm-10"
-                   label="Say hello to:"
-                   value={name}
-                   onChange={this._handleChange}/>
+            <Input
+              type="text"
+              labelClassName="col-sm-2"
+              wrapperClassName="col-sm-10"
+              label="Say hello to:"
+              value={name}
+              onChange={this.handleChange}
+            />
         </form>
       </div>
     );
