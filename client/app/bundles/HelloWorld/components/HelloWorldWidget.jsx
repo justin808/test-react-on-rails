5f6232048edc40e0a60646cf507315a05651d22f
Justin Gordon <justin.gordon@gmail.com>
2016-01-04 22:21:06 -0800

    nicer looking hello world

--- client/app/bundles/HelloWorld/components/HelloWorldWidget.jsx
+++ client/app/bundles/HelloWorld/components/HelloWorldWidget.jsx
@@ -1,17 +1,17 @@
-// HelloWorldWidget is an arbitrary name for any "dumb" component. We do not recommend suffixing all your
-// dump component names with Widget.
+// HelloWorldWidget is an arbitrary name for any "dumb" component. We do not recommend suffixing
+// all your dump component names with Widget.
 
 import React, { PropTypes } from 'react';
 import Immutable from 'immutable';
 import _ from 'lodash';
-import { Row, Col } from 'react-bootstrap';
+import { Input, Row, Col } from 'react-bootstrap';
 
 // Simple example of a React "dumb" component
 export default class HelloWorldWidget extends React.Component {
   static propTypes = {
     // We prefix all property and variable names pointing to Immutable.js objects with '$$'.
-    // This allows us to immediately know we don't call $$helloWorldStore['someProperty'], but instead use
-    // the Immutable.js `get` API for Immutable.Map
+    // This allows us to immediately know we don't call $$helloWorldStore['someProperty'], but
+    // instead use the Immutable.js `get` API for Immutable.Map
     actions: PropTypes.shape({
       updateName: PropTypes.func.isRequired,
     }).isRequired,
@@ -37,18 +37,19 @@ export default class HelloWorldWidget extends React.Component {
     const $$helloWorldStore = this.props.$$helloWorldStore;
     const name = $$helloWorldStore.get('name');
     return (
-      <div className="container">
+      <div>
         <h3>
           Hello, {name}!
         </h3>
-        <Row>
-          <Col sm={2}>
-            Say hello to:
-          </Col>
-          <Col sm={10}>
-            <input type="text" value={name} onChange={this._handleChange} />
-          </Col>
-        </Row>
+        <hr/>
+        <form className="form-horizontal">
+            <Input type="text"
+                   labelClassName="col-sm-2"
+                   wrapperClassName="col-sm-10"
+                   label="Say hello to:"
+                   value={name}
+                   onChange={this._handleChange}/>
+        </form>
       </div>
     );
   }
